3ba7dee80e88332e0ae768e965ae3fdfbd2e0d92
Holly <papiniu@outlook.com>
2016-12-09 19:55:10 -0800

    starting to set up PHP for about page

--- themes/dumplinghub_site/page-templates/about.php
+++ themes/dumplinghub_site/page-templates/about.php
@@ -1,22 +1,16 @@
 <?php /* Template Name: About Page */ ?>
 <?php get_header(); ?>
-
-   <section class="about-hero">
-    <h6> about me </h6>
-   </section>
-
-   <div class= "about-section">
-            <p>
-         
-               <?php echo CFS()->get( 'our_story'); ?></p>
-
-               <p>
-               
-            
-               <?php echo CFS()->get( 'our_team'); ?></p>
-
-
-
-	</div><!-- #primary -->
-
+<section class="about-hero">
+    <h6>
+        <?php the_title();?> </h6>
+</section>
+<div class="about-section">
+    <p>
+        <?php echo CFS()->get( 'description'); ?>
+    </p>
+    <p>
+        <?php echo CFS()->get( 'values'); ?>
+    </p>
+</div>
+<!-- #primary -->
 <?php get_footer(); ?>
