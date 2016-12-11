223dc498c4d71e2833e4e4da11db5a6e9c8389e6
Holly <papiniu@outlook.com>
2016-12-11 11:43:24 -0800

    Renamed single project and taxonomy project page

---
+++ themes/dumplinghub_site/taxonomy-project_type.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying archive project type pages.
+ *
+ * @package RED_Starter_Theme
+ */
+get_header(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<?php if ( have_posts() ) : ?>
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header>
+			<!-- .page-header -->
+			<div class="boxes" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<?php while ( have_posts() ) : the_post(); ?>
+				<div class="project-post">
+					<div class="project-thumbnail">
+						<a href="<?php the_permalink() ; ?> ">
+							<?php the_post_thumbnail( 'large' ); ?>
+						</a>
+					</div>
+					<div class="project-info">
+						<?php the_title('<p class="project-title">'); ?>........
+						<?php echo CFS()->get( 'project_price' ); ?>
+					</div>
+				</div>
+				<!-- project-post -->
+				<?php endwhile ?>
+			</div>
+	</div>
+	<!-- #post-## -->
+	</div>
+	<?php else : ?>
+	<?php get_template_part( 'template-parts/content', 'none' ); ?>
+	<?php endif; ?>
+	</main>
+	<!-- #main -->
+	</div>
+	<!-- #primary -->
+	<?php get_footer(); ?>
