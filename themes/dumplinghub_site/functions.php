1b27b2f14f76120e7adff60417122c3c700d4ba8
Holly <papiniu@outlook.com>
2016-12-10 21:24:27 -0800

    Connected javascript file  in functions, created script.js

--- themes/dumplinghub_site/functions.php
+++ themes/dumplinghub_site/functions.php
@@ -84,8 +84,11 @@ add_filter( 'stylesheet_uri', 'dumplinghub_site_minified_css', 10, 2 );
  */
 function dumplinghub_site_scripts() {
 	wp_enqueue_style( 'dumplinghub-site-style', get_stylesheet_uri() );
-
+	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'); 
 	wp_enqueue_script( 'dumplinghub-site-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
+	wp_enqueue_script( 'jquery');
+	wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
