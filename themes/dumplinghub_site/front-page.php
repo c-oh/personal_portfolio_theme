90a0e4d8a2e611fb72eb3e83ef2c2bcc9a812bfb
Holly <papiniu@outlook.com>
2016-12-11 11:28:56 -0800

    Added summary code snippet

--- themes/dumplinghub_site/front-page.php
+++ themes/dumplinghub_site/front-page.php
@@ -5,7 +5,32 @@
  * @package dumplinghub-site_Theme
  */
 get_header(); ?>
+<!--typesection-->
   <div class="text">
     <h3 class="typing-animation"> I am <span id="typewrite"> </span></h3>
   </div>
+
+  <!--Journal section-->
+  <section class="journal">
+    <h2>journal</h2>
+    <div class="journal-container">
+      <?php
+   $args = array( 'numberposts' => '3', 'order' => 'DESC','post_status' => 'publish'  );
+   $recent_posts = get_posts( $args ); // returns an array of posts ?>
+        <?php foreach ( $recent_posts as $post ) : setup_postdata( $post ); ?>
+        <div class="post-list-info">
+          <?php the_post_thumbnail('category-thumb'); ?>
+          <div class="info-area">
+            <?php the_date(); ?>/
+            <?php comments_number();?>
+            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class='title'>
+              <?php the_title();?> </a>
+            <a href="<?php the_permalink(); ?>" class='read-entry-btn'> Read More</a>
+          </div>
+        </div>
+        <?php endforeach; wp_reset_postdata(); ?>
+    </div>
+  </section>
+
+  <!--footer-->
   <?php get_footer();?>
