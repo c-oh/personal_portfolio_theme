371ca481fb56023b5a24cc6c8652ed5bc752b541
Holly <papiniu@outlook.com>
2016-12-13 15:47:05 -0800

    Added journal picture

--- themes/dumplinghub_site/footer.php
+++ themes/dumplinghub_site/footer.php
@@ -11,13 +11,13 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="footer-container">
 			<p>
-				<a href="<?php echo esc_url( 'https://github.com/dumplinghub' ); ?>"> <i class="fa fa-github" aria-hidden="true"></i></a>
+				<a href="<?php echo esc_url( 'https://github.com/dumplinghub' ); ?>">  <i class="fa fa-github" aria-hidden="true"></i></a>
 			</p>
 			<p>
 				<a href="<?php echo esc_url( 'https://www.linkedin.com/in/holly-cheng-1320b4129' ); ?>"> <i class="fa fa-linkedin" aria-hidden="true"></i></a>
 			</p>
 			<p>
-				<a href="<?php echo esc_url( 'adfsd@afads.com' ); ?>"> <i class="fa fa-envelope" aria-hidden="true"></i> </a>
+				<a href="<?php echo esc_url( 'adfsd@afads.com' ); ?>">  <i class="fa fa-envelope" aria-hidden="true"></i> </a>
 			</p>
 		</div>
 		<div class = "copyright">
--- themes/dumplinghub_site/front-page.php
+++ themes/dumplinghub_site/front-page.php
@@ -5,33 +5,33 @@
  * @package dumplinghub-site_Theme
  */
 get_header(); ?>
-<!--typesection-->
+  <!--typesection-->
   <div class="text">
     <h1 class="typing-animation"> <span id="typewrite"> </span></h1>
   </div>
-
-<!--show casing work-->
-<section class="taxonomy-loop">
-        <h2> Projects </h2>
-        <?php 
+  <!--show casing work-->
+  <section class="taxonomy-loop">
+    <h2> Projects </h2>
+    <?php 
 		$taxonomies  = get_terms( array(
 			'taxonomy' => 'project_type',
 			'hide_empty' => true,
 			) );
 			foreach ( $taxonomies as $term ):?>
-        <div class="taxonomy-loop-wrapper">
-          <div class="taxonomy-text-container">
-            <?php $url = get_term_link($term->slug, 'project_type') ?>
-            <h4> <?php echo $term->description ?> </h4>
-               <h5> <?php echo $term->name ?> </h5>
-                 <a href="<?php echo $url ?>">
-                  <p> View Projects </p>
-                 </a>
+    <div class="taxonomy-loop-wrapper">
+      <div class="taxonomy-text-container">
+        <?php $url = get_term_link($term->slug, 'project_type') ?>
+        <h4>
+          <?php echo $term->description ?> </h4>
+        <h5>
+          <?php echo $term->name ?> </h5>
+        <a href="<?php echo $url ?>">
+          <p> View Projects </p>
+        </a>
       </div>
-        </div>
-        <?php endforeach ?>
-        </section>
-
+    </div>
+    <?php endforeach ?>
+  </section>
   <!--Journal section-->
   <section class="journal">
     <h2>journal</h2>
@@ -41,17 +41,16 @@ get_header(); ?>
    $recent_posts = get_posts( $args ); // returns an array of posts ?>
         <?php foreach ( $recent_posts as $post ) : setup_postdata( $post ); ?>
         <div class="post-list-info">
-          <?php the_post_thumbnail('category-thumb'); ?>
+          <a href="<?php the_permalink(); ?>" title="" class='blog-picture'>
+            <?php the_post_thumbnail('category-thumb');  ?> </a>
           <div class="info-area">
-            <?php the_date(); ?>/
             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class='title'>
               <?php the_title();?> </a>
-            <a href="<?php the_permalink(); ?>" class='read-entry-btn'> Read More</a>
+            <?php the_date(); ?>
           </div>
         </div>
         <?php endforeach; wp_reset_postdata(); ?>
     </div>
   </section>
-
   <!--footer-->
   <?php get_footer();?>
