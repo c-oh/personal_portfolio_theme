fee3c9f1db8e317f2789a27c350da59b9402d324
hollythedev <papiniu@outlook.com>
2016-12-31 02:27:09 -0800

    Set up CFS for resume

--- themes/dumplinghub_site/page-templates/resume.php
+++ themes/dumplinghub_site/page-templates/resume.php
@@ -3,13 +3,26 @@
 
 <section class= "resume-content">
     <div class= "resume-title">
-        <h1><?php echo CFS()->get( 'defintion'); ?></h1>
-    </div>
+        <h1><?php echo CFS()->get( 'resume_title'); ?></h1>
+    
 
 <form method="get" action="/../assets/files/file.doc">
 <button type="submit">Download Resume</button>
 </form>
+</div>
 
+<div class= "work-experience">
+    <?php echo CFS()->get( 'work_experience'); ?>
+</div>
+<div class= "skills">
+    <?php echo CFS()->get( 'skills'); ?>
+</div>
+<div class= "soft-skills">
+    <?php echo CFS()->get( 'soft_skills'); ?>
+</div>
+<div class= "education">
+    <?php echo CFS()->get( 'education'); ?>
+</div>
 </section>
 <!-- #primary -->
 <?php get_footer(); ?>
