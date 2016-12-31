28652c0eec62cff2f963a6f947849181500ba5ca
hollythedev <papiniu@outlook.com>
2016-12-31 12:23:41 -0800

    Added sections and divs to resume descriptions

--- themes/dumplinghub_site/page-templates/resume.php
+++ themes/dumplinghub_site/page-templates/resume.php
@@ -14,16 +14,32 @@
 
 <section class= "resume-body">
 <div class= "work-experience">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/screwdriver.png'; ?>"  alt="Icon of gear" />
+  <div> 
+    <h1> <?php echo CFS()->get( 'work_experience_header'); ?> </h1>
     <?php echo CFS()->get( 'work_experience'); ?>
 </div>
+</div>
 <div class= "skills">
-    <?php echo CFS()->get( 'skills'); ?>
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/pen.png'; ?>"  alt="Icon of pen" />
+  <div>
+   <h1><?php echo CFS()->get( 'skills_header'); ?></h1>
+    <p> <?php echo CFS()->get( 'skills'); ?></p>
+    </div>
 </div>
 <div class= "soft-skills">
-    <?php echo CFS()->get( 'soft_skills'); ?>
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/paint.png'; ?>"  alt="Icon of paint board" />
+  <div> 
+   <h1><?php echo CFS()->get( 'soft_skills_header'); ?></h1>
+    <p><?php echo CFS()->get( 'soft_skills'); ?></p>
+</div>
 </div>
 <div class= "education">
-    <?php echo CFS()->get( 'education'); ?>
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/hat.png'; ?>"  alt="Icon of hat" />
+  <div>
+  <h1> <?php echo CFS()->get( 'education_header'); ?> </h1>
+   <p> <?php echo CFS()->get( 'education'); ?> </p>
+</div>
 </div>
 </section>
 <!-- #primary -->
