b09d5e626a051fca80fffa016291f7aeb7bf1407
hollythedev <papiniu@outlook.com>
2016-12-31 02:22:43 -0800

    Added download button, added text and custom fields to php file for about

--- themes/dumplinghub_site/page-templates/about.php
+++ themes/dumplinghub_site/page-templates/about.php
@@ -3,9 +3,20 @@
 <?php get_header(); ?>
 
 
+<section class= "about-content">
+<div class="text-section">
+<p class= "defintion">
+ <?php echo CFS()->get( 'defintion'); ?>
+ </p>
 
-
-
+ <p class= "about_description">
+     <?php echo CFS()->get( 'about_description'); ?>
+     </p>
+</div>
+<div class= "about-picture">
+<h1>picture of a map</h1>
+</div>
+ </section>
 
 <!-- #primary -->
 <?php get_footer(); ?>
--- themes/dumplinghub_site/page-templates/resume.php
+++ themes/dumplinghub_site/page-templates/resume.php
@@ -1,98 +1,14 @@
 <?php /* Template Name: Resume Page */ ?>
 <?php get_header(); ?>
-<section class="resume-hero">
-    <h6> resume </h6>
-</section>
-<section class="skill-section">
-    <h2>
-        <?php echo CFS()->get( 'titles'); ?>
-    </h2>
-    <h3>
-        <?php echo CFS()->get( 'level'); ?> </h3>
-    <p>
-        <?php echo CFS()->get( 'skills'); ?>
-    </p>
-    <h3>
-        <?php echo CFS()->get( 'level1'); ?> </h3>
-    <p>
-        <?php echo CFS()->get( 'skills1'); ?>
-    </p>
-</section>
 
-<!--Work experience section-->
-<section class="work-experience">
-    <div class= "work-title">
-    <h2>
-        <?php echo CFS()->get( 'work_experience'); ?>
-    </h2>
+<section class= "resume-content">
+    <div class= "resume-title">
+        <h1><?php echo CFS()->get( 'defintion'); ?></h1>
     </div>
 
-    <div class= "resume-container">
-    <h3>
-        <?php echo CFS()->get( 'position'); ?>
-    </h3>
-    <h4>
-        <?php echo CFS()->get( 'location_time'); ?>
-    </h4>
-    <p>
-        <?php echo CFS()->get( 'description'); ?>
-    </p>
-    </div>
-
-     <div class= "resume-container">
-    <h3>
-        <?php echo CFS()->get( 'position1'); ?>
-    </h3>
-    <h4>
-        <?php echo CFS()->get( 'location_time1'); ?>
-    </h4>
-    <p>
-        <?php echo CFS()->get( 'description1'); ?>
-    </p>
-    </div>
-
-     <div class= "resume-container">
-    <h3>
-        <?php echo CFS()->get( 'position2'); ?>
-    </h3>
-    <h4>
-        <?php echo CFS()->get( 'location_time2'); ?>
-    </h4>
-    <p>
-        <?php echo CFS()->get( 'description2'); ?>
-    </p>
-    </div>
-
-</section>
-
-<!---education -->
-
-<section class= "education">
-<div class= "education-title">
-    <h2>
-        <?php echo CFS()->get( 'education'); ?>
-    </h2>
-    </div>
-
-<div class= "resume-container">
-    <h2>
-        <?php echo CFS()->get( 'major'); ?>
-    </h2>
-    <h4>
-        <?php echo CFS()->get( 'where_when'); ?>
-    </h4>
-   
-</div>
-
-<div class= "resume-container">
-    <h2>
-        <?php echo CFS()->get( 'major1'); ?>
-    </h2>
-    <h4>
-        <?php echo CFS()->get( 'where_when1'); ?>
-    </h4>
-   
-</div>
+<form method="get" action="/../assets/files/file.doc">
+<button type="submit">Download Resume</button>
+</form>
 
 </section>
 <!-- #primary -->
