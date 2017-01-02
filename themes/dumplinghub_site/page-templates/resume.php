13e0ee843e75565939b1e10c1c1fa1c7c361af1b
hollythedev <papiniu@outlook.com>
2017-01-01 18:17:56 -0800

    Added sass to acordian

--- themes/dumplinghub_site/page-templates/resume.php
+++ themes/dumplinghub_site/page-templates/resume.php
@@ -13,27 +13,27 @@
 <section class="resume-body">
 
 <div class="work-experience">
-<button class="accordion"> <img src="<?php echo get_template_directory_uri() . '/assets/images/screwdriver.png'; ?>" alt="Icon of gear" /> <?php echo CFS()->get( 'work_experience_header'); ?> </button>
+<button class="accordion"> <div><img src="<?php echo get_template_directory_uri() . '/assets/images/screwdriver.png'; ?>" alt="Icon of gear" /> <p> <?php echo CFS()->get( 'work_experience_header'); ?> </div></p></button>
 <div class="panel">
   <p>    <?php echo CFS()->get( 'work_experience'); ?></p>
 </div>
  </div>
 
 <div class="skills">
-<button class="accordion"> <img src="<?php echo get_template_directory_uri() . '/assets/images/pen.png'; ?>" alt="Icon of pen" />  <?php echo CFS()->get( 'skills_header'); ?></button>
+<button class="accordion"><div> <div><img src="<?php echo get_template_directory_uri() . '/assets/images/pen.png'; ?>" alt="Icon of pen" />  <p><?php echo CFS()->get( 'skills_header'); ?> </p></div></button>
 <div class="panel">
   <p><?php echo CFS()->get( 'skills'); ?></p>
 </div>
 
  <div class="soft-skills">
-<button class="accordion"> <img src="<?php echo get_template_directory_uri() . '/assets/images/paint.png'; ?>" alt="Icon of paint board" />   <?php echo CFS()->get( 'soft_skills_header'); ?> </button>
+<button class="accordion"> <div><img src="<?php echo get_template_directory_uri() . '/assets/images/paint.png'; ?>" alt="Icon of paint board" />  <p> <?php echo CFS()->get( 'soft_skills_header'); ?>  </p></div></button>
 <div class="panel">
   <p><?php echo CFS()->get( 'soft_skills'); ?></p>
 </div>
 </div>
 
  <div class="education">
-<button class="accordion"> <img src= "<?php echo get_template_directory_uri() . '/assets/images/hat.png'; ?>" alt="Icon of hat" />   <?php echo CFS()->get( 'education_header'); ?> </h1></button>
+<button class="accordion"> <div><img src= "<?php echo get_template_directory_uri() . '/assets/images/hat.png'; ?>" alt="Icon of hat" />  <p> <?php echo CFS()->get( 'education_header'); ?> </p> </div></button>
 <div class="panel">
   <p><?php echo CFS()->get( 'education'); ?></p>
 </div>
